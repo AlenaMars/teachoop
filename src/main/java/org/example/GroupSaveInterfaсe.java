1a629ff7e327222df40efd11bd451eed4bc75535
Alena <alenap89@mail.ru>
2023-03-28 11:39:39 +0700

    Initial commit

---
+++ src/main/java/org/example/GroupSaveInterfaсe.java
@@ -0,0 +1,11 @@
+
+package org.example;
+import java.io.IOException;
+
+public interface GroupSaveInterface {
+
+    public void saveGroup(Group group)throws IOException;
+
+    public Group loadGroup() throws IOException;
+
+}
